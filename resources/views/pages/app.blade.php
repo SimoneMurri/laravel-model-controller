d655b1eee6e03897ae5853d7be26ac3d98adda6d
Simone Murri <simonemurri701@gmail.com>
2024-02-02 01:04:54 +0100

    stampate le card in pagina usando il foreach

--- resources/views/pages/app.blade.php
+++ resources/views/pages/app.blade.php
@@ -8,12 +8,25 @@
     @vite('resources/js/app.js')
 </head>
 <body>
-    <ul>
-        @foreach ($movies as $movie)
-        <li>
-            {{ $movie->title }}
-        </li>
-        @endforeach
+  <div class="container">
+    <div class="row">
+    @foreach ($movies as $movie)
+       <div class="col-md-4">
+        <div class="card mb-3">
+    <div class="card-header">
+        {{ $movie->id }}
+    </div>
+    <ul class="list-group list-group-flush">
+            <li class="list-group-item">{{ $movie->title }}</li>
+            <li class="list-group-item">{{ $movie->original_title }}</li>
+            <li class="list-group-item">{{ $movie->nationality }}</li>
+            <li class="list-group-item">{{ $movie->date }}</li>
+            <li class="list-group-item">{{ $movie->vote }}</li>
     </ul>
+      </div>
+    </div>
+    @endforeach
+  </div>
+</div>
 </body>
 </html>
